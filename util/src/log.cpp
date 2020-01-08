45710d27f039a624c8e6564661fe6af9f64e59ad
solopointer <solopointer1202@gmail.com>
2020-01-08 14:01:04 +0800

    update log

--- util/src/log.cpp
+++ util/src/log.cpp
@@ -3,20 +3,19 @@
 #include "log.h"
 
 
-void log(LOG_LEVEL loglevel, const char * fmt, ...)
-{
+void log(LOG_LEVEL loglevel, const char * file, unsigned int line, const char *fmt, ...) {
     va_list args;
     char buf[2048];
     va_start(args, fmt);
     vsprintf(buf, fmt, args);
     va_end(args);
     switch(loglevel) {
-    case LOG_LEVEL::TRACE: BOOST_LOG_TRIVIAL(trace) << buf;break;
-    case LOG_LEVEL::DEBUG: BOOST_LOG_TRIVIAL(debug) << buf;break;
-    case LOG_LEVEL::INFO: BOOST_LOG_TRIVIAL(info) << buf;break;
-    case LOG_LEVEL::WARNING: BOOST_LOG_TRIVIAL(warning) << buf;break;
-    case LOG_LEVEL::ERROR: BOOST_LOG_TRIVIAL(error) << buf;break;
-    case LOG_LEVEL::FATAL: BOOST_LOG_TRIVIAL(fatal) << buf;break;
+    case LOG_LEVEL::TRACE: BOOST_LOG_TRIVIAL(trace)<<"["<<file<<"]["<<line<<"]"<< buf;break;
+    case LOG_LEVEL::DEBUG: BOOST_LOG_TRIVIAL(debug)<<"["<<file<<"]["<<line<<"]"<< buf;break;
+    case LOG_LEVEL::INFO: BOOST_LOG_TRIVIAL(info)<<"["<<file<<"]["<<line<<"]"<< buf;break;
+    case LOG_LEVEL::WARNING: BOOST_LOG_TRIVIAL(warning)<<"["<<file<<"]["<<line<<"]"<< buf;break;
+    case LOG_LEVEL::ERROR: BOOST_LOG_TRIVIAL(error)<<"["<<file<<"]["<<line<<"]"<< buf;break;
+    case LOG_LEVEL::FATAL: BOOST_LOG_TRIVIAL(fatal)<<"["<<file<<"]["<<line<<"]"<< buf;break;
     };
 }
 
