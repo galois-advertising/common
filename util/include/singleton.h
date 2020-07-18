90e72dd3675df06d182fc631bba1cb5361446be4
solopointer <solopointer1202@gmail.com>
2020-07-18 17:48:43 +0800

    update singleton;fix multithread bug

--- util/include/singleton.h
+++ util/include/singleton.h
@@ -1,21 +1,30 @@
 #include <memory>
 #include <mutex>
+#include <atomic>
 
 namespace galois::common {
+/**************************************************
+§6.7 [stmt.dcl] p4
+C++11 removes the need for manual locking. 
+Concurrent execution shall wait if a static local variable is already being initialized.
+If control enters the declaration concurrently while the variable is being initialized, 
+the concurrent execution shall wait for completion of the initialization.
+**************************************************/
 template<class T>
-class singleton
-{
+T* get_singleton() {
+    static T _s_instance;
+    return &_s_instance;
+};
+
+template<class T>
+class singleton {
 public:
     template <class...args_types> 
-    static std::shared_ptr<T> instance(args_types...args)
-    {
+    static T* instance(args_types...args) {
         if (nullptr == _s_instance) {
             std::lock_guard<std::mutex> guard(_mtx);
             if (nullptr == _s_instance) {
-                _s_instance = std::make_shared<T>(args...);
-                if (nullptr == _s_instance) {
-                    return nullptr;
-                }
+                _s_instance = new T(args...);
             }
         }
         return _s_instance;
@@ -26,11 +35,11 @@ public:
     singleton & operator=(const singleton&) = delete;
 
 private:
-    static std::shared_ptr<T> _s_instance;
+    static std::atomic<T*> _s_instance;
     static std::mutex _mtx;
 };
 template<class T>
-std::shared_ptr<T> singleton<T>::_s_instance = nullptr;
+std::atomic<T*> singleton<T>::_s_instance = nullptr;
 
 template<class T>
 std::mutex singleton<T>::_mtx;
--- util/test/singleton.cpp
+++ util/test/singleton.cpp
@@ -2,15 +2,16 @@
 #include "singleton.h"
 #include <gtest/gtest.h>
 
-class test
-{
+class test {
     int aa;
     float bb;
     double cc;
 public:
+    test() : aa(0), bb(0.0), cc(0.0) {
+
+    }
     test(int a, float b, double c) : 
-        aa(a), bb(b), cc(c)
-    {
+        aa(a), bb(b), cc(c) {
     };
     void print() {
         std::cout<<aa<<"|"<<bb<<"|"<<cc<<std::endl;
@@ -22,5 +23,6 @@ TEST(Test, singleton) {
     using namespace galois::common;
     auto inst = singleton<test>::instance(1, 2.3, 3.4);
     inst->print();
-
+    auto inst2 = get_singleton<test>();
+    inst2->print();
 }
